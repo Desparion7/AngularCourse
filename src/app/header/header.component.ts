cc5b8eca0f8c1b2e17caee58559be8541150465f
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-08-22 17:59:52 +0200

    add conection to firebase

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  constructor() {}
+  constructor(private dataStorageService: DataStorageService) {}
 
   ngOnInit(): void {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipe();
+  }
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,20 +8,25 @@ import { Subject } from 'rxjs-compat';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
   private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/3:2/w_3000,h_2000,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('Cheese', 1)]
-    ),
-    new Recipe(
-      'A Test Recipe2',
-      'This is second recipe',
-      'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/08/chana-masala-recipe-500x500.jpg',
-      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
-    ),
+    // new Recipe(
+    //   'A Test Recipe',
+    //   'This is simply a test',
+    //   'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/3:2/w_3000,h_2000,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg',
+    //   [new Ingredient('Meat', 1), new Ingredient('Cheese', 1)]
+    // ),
+    // new Recipe(
+    //   'A Test Recipe2',
+    //   'This is second recipe',
+    //   'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/08/chana-masala-recipe-500x500.jpg',
+    //   [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+    // ),
   ];
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
